7f33616c4eac6c968407124bfd20db51ca1abd87
cfx78 <104265674+cfx78@users.noreply.github.com>
2022-10-24 04:42:56 -0500

    Adding Async Await section

--- async.js
+++ async.js
@@ -1,38 +1,53 @@
 // Promises
 
-const orderSubmit = new Promise((resolve, reject) => {
-	let orderCompleted = true;
+function orderSubmit() {
+	new Promise((resolve, reject) => {
+		let orderCompleted = false;
 
-	if (orderCompleted) {
-		resolve('Submitted!');
-	} else {
-		reject('Please try again');
-	}
-});
-
-orderSubmit
-	.then((result) => {
-		console.log(result);
+		if (orderCompleted) {
+			resolve('Submitted!');
+		} else {
+			reject('Please try again');
+		}
 	})
-	.catch((result) => {
-		console.log(result);
-	});
+		.then((result) => {
+			console.log(result);
+		})
+		.catch((result) => {
+			console.log(result);
+		});
+}
 
-let remainingTime = new Promise((resolve, reject) => {
-	let trackProgress = 0.53;
+function remainingTime() {
+	new Promise((resolve, reject) => {
+		let trackProgress = 0.53;
 
-	if (trackProgress === 0) {
-		resolve('Next track');
-	} else {
-		reject('Keep Playing');
-	}
-});
-
-remainingTime
-	.then((response) => {
-		console.log(response);
+		if (trackProgress === 0) {
+			resolve('Next track');
+		} else {
+			reject('Keep Playing');
+		}
 	})
-	.catch((response) => {
-		console.log(response);
-	});
-4;
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((response) => {
+			console.log(response);
+		});
+}
+
+orderSubmit();
+remainingTime();
+
+// Async Await
+
+async function getName() {
+	try {
+		let name = prompt('Please enter a name');
+		await console.log('Hi ' + name);
+	} catch (error) {
+		console.log('Something went wrong');
+	}
+}
+
+getName();
